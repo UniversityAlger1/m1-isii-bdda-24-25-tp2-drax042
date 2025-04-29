22dce0aa34352b0307dd545808c85d7762479bba
drax042 <148236404+drax042@users.noreply.github.com>
2025-04-29 16:02:50 +0100

    Update 3_1_oldest.sql

--- 3_1_oldest.sql
+++ 3_1_oldest.sql
@@ -1,1 +1,11 @@
+-- Use the correct database
+USE LabDatabase;
+GO
 
+-- Display the name of the oldest chercheur
+SELECT NomCh
+FROM Chercheur
+WHERE date_naissance = (
+    SELECT MIN(date_naissance)
+    FROM Chercheur
+);
