186a40646bd8dad2feae3472a816cb27311a60da
drax042 <148236404+drax042@users.noreply.github.com>
2025-04-29 16:03:15 +0100

    Update 3_2_recent.sql

--- 3_2_recent.sql
+++ 3_2_recent.sql
@@ -1,1 +1,11 @@
+-- Use the correct database
+USE LabDatabase;
+GO
 
+-- Display the name of the most recently created laboratoire
+SELECT NomLab
+FROM Laboratoire
+WHERE Datcreation = (
+    SELECT MAX(Datcreation)
+    FROM Laboratoire
+);
